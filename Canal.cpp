465f93b5babe930ffd09f7504edc091b942015f2
Carlos <a01425194@tec.mx>
2026-06-13 23:57:02 -0600

    canal

---
+++ Canal.cpp
@@ -0,0 +1,50 @@
+#include "Canal.h"
+#include <iostream>
+using namespace std;
+
+Canal::Canal(int _idCanal, string _nombre, int _suscriptores)
+{
+    idCanal = _idCanal;
+    nombre = _nombre;
+    suscriptores = _suscriptores;
+}
+
+void Canal::agregarVideo(Video *video)
+{
+    if (video)
+    {
+        videos.push_back(video);
+    }
+}
+
+void Canal::mostrarInfo() const
+{
+    cout << "=== CANAL ===" << endl;
+    cout << "ID: " << idCanal << endl;
+    cout << "Nombre: " << nombre << endl;
+    cout << "Suscriptores: " << suscriptores << endl;
+    cout << "Videos publicados: " << videos.size() << endl;
+}
+
+void Canal::mostrarVideos() const
+{
+    cout << " Videos del canal [" << nombre << "] ---" << endl;
+    for (Video *v : videos)
+    {
+        v->mostrarInfo();
+        cout << endl;
+    }
+}
+
+int Canal::getIdCanal() const
+{
+    return idCanal;
+}
+string Canal::getNombre() const
+{
+    return nombre;
+}
+int Canal::getSuscriptores() const
+{
+    return suscriptores;
+}
